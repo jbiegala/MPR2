07dca64ffd32a38a96474cd5d14f71de1075e4a9
jbiegala <jbiegala@gmail.com>
2021-02-04 13:13:12 +0100

    initial project

---
+++ src/main/java/pl/pjatk/pozyczto/PozyczToApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.pozyczto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PozyczToApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PozyczToApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/pozyczto/PozyczToApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.pozyczto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PozyczToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
